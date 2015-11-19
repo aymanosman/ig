b945f14041307d5118f38ad854392661832bdda4
Ayman Osman <aymano.osman@gmail.com>
2015-11-19 00:26:02 +0000

    wip

--- examples/00_SimpleHaskell/Main.hs
+++ examples/00_SimpleHaskell/Main.hs
@@ -1,18 +1,42 @@
 -- {-# LANGUAGE OverloadedStrings #-}
 
 import Network.Wreq
+import Control.Lens
+import Data.Aeson (toJSON)
+import Data.Aeson.Lens (key, nth)
+import System.Environment
 
 
 base :: String
 base = "https://demo-api.ig.com/gateway/deal"
 
 
-run =
+(=:) :: String -> String -> (String, String)
+(=:) = (,)
+
+
+run :: String -> IO ()
+run username =
+  let -- json :: Value
+      json =
+        toJSON
+          [ "identifier" =: username
+          ]
+      -- opts = defaults & header "Accept" .~ ["*/*"]
+  in
   do r <- post (base ++ "/session")
-          ["identifier" := "aosman"]
-     return r
-
-
-main =
-  do _ <- run
+          -- ["identifier" := username]
+          json
+     print r
      return ()
+
+
+main :: IO ()
+main =
+  do args <- getArgs
+     case args of
+       [username] ->
+         do _ <- run username
+            return ()
+       _ ->
+         fail "Usage: main USERNAME"
