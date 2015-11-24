4d9dfc01360a29b55e8002f13f1627cb908fbb4d
Ayman Osman <aymano.osman@gmail.com>
2015-11-24 22:27:18 +0000

    from ini

--- examples/00_SimpleHaskell/Main.hs
+++ examples/00_SimpleHaskell/Main.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
-
 import Control.Exception (catch)
+import Control.Monad ((<=<))
 import Control.Lens hiding ((.=))
 import Data.Aeson (toJSON, (.=), object)
 import Network.Wreq
@@ -8,11 +8,47 @@ import Data.Aeson.Lens (key, nth)
 import System.Environment
 import Data.ByteString.Char8 as BS
 import Data.ByteString.Lazy.Char8 as LBS
+import Data.Maybe (isJust, fromJust)
 import qualified Network.HTTP.Client as HTTP
+import qualified Data.Ini as Ini
+import qualified Data.Text as Text
+import qualified Data.List as List
 
 
 base :: String
 base = "https://demo-api.ig.com/gateway/deal"
+
+
+-- return Creds (username, password, api key)
+getCreds filename =
+  either
+    fail
+    getCredsFromIni
+    =<<
+    Ini.readIniFile filename
+
+
+getCredsFromIni ini =
+  let mp = lookup' "password" ini
+      mu = lookup' "username" ini
+      mk = lookup' "api_key" ini
+  in
+  if List.all isJust [mu, mp, mk] then
+    return $ Just
+      ( fromJust mu
+      , fromJust mp
+      , fromJust  mk
+      )
+  else
+    return Nothing
+
+
+lookup' key ini =
+  case Ini.lookupValue "default" key ini of
+    Right p ->
+      Just (Text.unpack p)
+    _ ->
+      Nothing
 
 
 (=:) :: String -> String -> (String, String)
@@ -22,7 +58,7 @@ base = "https://demo-api.ig.com/gateway/deal"
 run
   :: String -- username
   -> String -- password
-  -> BS.ByteString -- api key
+  -> String -- api key
   -> IO ()
 
 test u p k =
@@ -38,12 +74,14 @@ mkPayload u p =
     , "password" .= p
     ]
 
+
 mkOpts apiKey =
   defaults
-    & header "X-IG-API-KEY".~ [apiKey]
+    & header "X-IG-API-KEY".~ [BS.pack apiKey]
     & header "Content-Type" .~ ["application/json; charset=UTF-8"]
     & header "Accept" .~ ["application/json; charset=UTF-8"]
     -- & header "Version" .~ ["2"]
+
 
 run username password apiKey =
   do r <- postWith (mkOpts apiKey) (base ++ "/session")
@@ -51,18 +89,28 @@ run username password apiKey =
      print r
      return ()
 
+
+run' u p k =
+  do _ <- run u p k
+     return ()
+
+
 handler e@(HTTP.StatusCodeException status respHeaders cookieJar) =
   do print status
      print respHeaders
+
 
 main :: IO ()
 main =
   do args <- getArgs
      case args of
-       [username] ->
-         let apiKey = "foo" -- BS
-         in
-         do _ <- run username "secret" apiKey
-            return ()
+       [anyfoo] ->
+         maybe
+           (fail "Failed to get creds from .env file")
+           (\(username, password, apiKey) ->
+              run' username password apiKey)
+          =<<
+          getCreds ".env"
+
        _ ->
-         fail "Usage: main USERNAME"
+         fail "Usage: main anyfoofornow"
